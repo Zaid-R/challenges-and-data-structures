cfdf1dd70b326f67c828a7f880dc0bdd44807f65
Zaid Rajab <23041515@student.ltuc.com>
2024-07-03 21:26:22 +0300

    solve challenge 6-B

---
+++ Challenges/Reverse-Words/Reverse Words Sln/ReverseWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseWords
+{
+    public class Program
+    {
+        public static string ReverseWords(string sentence)
+        {
+            string[] words = sentence.Split(" ");
+            int wordsLength = words.Length;
+
+            string[] reversedWords = new string[words.Length];
+            for (int i = wordsLength; i > 0; i--)
+            {
+                reversedWords[wordsLength - i] = words[i - 1];
+            }
+            return string.Join(' ', reversedWords);
+        }
+
+        static void Main(string[] args)
+        {
+            string input = "csharp is programming language";
+            Console.WriteLine("Input: {0} | output: {1}",input,ReverseWords(input));
+        }
+    }
+
+
+}
